3d8df86d57dbd02c127f32ac795c16d8ca693531
BlakeABos <blakbos811@ed.amdsb.ca>
2016-09-29 13:39:28 -0400

    Add project files.

---
+++ GreatingCardBlake/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GreatingCardBlake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.aries = new System.Windows.Forms.PictureBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.greetingButton = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.aries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // aries
+            // 
+            this.aries.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.aries.ErrorImage = global::GreatingCardBlake.Properties.Resources.aries_zodiac_sign;
+            this.aries.Image = global::GreatingCardBlake.Properties.Resources.download;
+            this.aries.InitialImage = global::GreatingCardBlake.Properties.Resources.aries_zodiac_sign;
+            this.aries.Location = new System.Drawing.Point(12, 12);
+            this.aries.Name = "aries";
+            this.aries.Size = new System.Drawing.Size(260, 195);
+            this.aries.TabIndex = 0;
+            this.aries.TabStop = false;
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.playButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.playButton.Location = new System.Drawing.Point(0, -3);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(286, 264);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "shhhhh";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // greetingButton
+            // 
+            this.greetingButton.Font = new System.Drawing.Font("Trajan Pro", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greetingButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.greetingButton.Location = new System.Drawing.Point(68, 219);
+            this.greetingButton.Name = "greetingButton";
+            this.greetingButton.Size = new System.Drawing.Size(152, 33);
+            this.greetingButton.TabIndex = 2;
+            this.greetingButton.Text = "Greetings !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.greetingButton);
+            this.Controls.Add(this.aries);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Happy Greatings";
+            ((System.ComponentModel.ISupportInitialize)(this.aries)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox aries;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label greetingButton;
+    }
+}
+
---
+++ GreatingCardBlake/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+    
+namespace GreatingCardBlake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            greetingButton.Visible = false;
+            playButton.Visible = false;
+            aries.Visible = false;
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.White, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            Pen drawPen3 = new Pen(Color.White, 5);
+            Pen drawPen2 = new Pen(Color.White, 3);
+            formGraphics.DrawEllipse(drawPen, 26, 68, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen, 151, 101, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen, 237, 159, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen, 244, 191, 10, 10);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 270, 241, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 2, 15, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 160, 35, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 90, 15, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 76, 69, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 69, 139, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 26, 149, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 150, 159, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 76, 130, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 120, 65, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 290, 15, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 262, 31, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 205, 50, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 200, 25, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 241, 130, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 138, 11, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 20, 18, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3,70, 100, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawEllipse(drawPen3, 160, 001, 5, 5);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen2, 151, 101, 244, 165);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen2, 26, 72, 151, 105);
+            Thread.Sleep(500);
+            formGraphics.DrawLine(drawPen2, 248, 189, 240, 162);
+            Graphics fg = this.CreateGraphics();
+            Font drawFont = new Font("Arial", 8, FontStyle.Regular);
+            fg.DrawString("Aries is one of the constellations of the ", drawFont, drawBrush, 5, 170);
+            fg.DrawString(" zodiac. It is located in the northern celestial .", drawFont, drawBrush, 5, 190);
+            fg.DrawString("hemisphere between Pisces to the west and Taurus", drawFont, drawBrush, 5, 210);
+            fg.DrawString(" to the east. The name Aries is Latin for ram, and its ", drawFont, drawBrush, 5, 230);
+            fg.DrawString(" symbol is (Unicode ♈), representing a ram's horns.", drawFont, drawBrush, 5, 250);
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Star);
+            player.Play();
+
+
+        }
+    }
+}
